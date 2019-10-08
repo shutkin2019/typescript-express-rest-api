68acb237535f76cacd508d406e607118e72efeb0
Andrey Shutkin <a.shutkin2019@mail.ru>
2019-10-09 05:07:48 +0700

    first commit

---
+++ src/components/Auth/index.ts
@@ -0,0 +1,70 @@
+import AuthService from './service';
+import HttpError from '@/config/error';
+import { NextFunction, Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { IUserModel } from '@/components/User/model';
+import app from '@/config/server/server';
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function signup(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const user: IUserModel = await AuthService.createUser(req.body);
+        const token: string = jwt.sign({ email: user.email }, app.get('secret'), {
+            expiresIn: '60m',
+        });
+
+        res.json({
+            token,
+            status: 200,
+            logged: true,
+            message: 'Sign in successfull',
+        });
+    } catch (error) {
+        if (error.code === 500) {
+            return next(new HttpError(error.message.status, error.message));
+        }
+        res.json({
+            status: 400,
+            message: error.message,
+        });
+    }
+}
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function login(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const user: IUserModel = await AuthService.getUser(req.body);
+
+        const token: string = jwt.sign({ email: user.email }, app.get('secret'), {
+            expiresIn: '60m',
+        });
+
+        res.json({
+            token,
+            status: 200,
+            logged: true,
+            message: 'Sign in successfull',
+        });
+    } catch (error) {
+        if (error.code === 500) {
+            return next(new HttpError(error.message.status, error.message));
+        }
+
+        res.json({
+            status: 400,
+            message: error.message,
+        });
+    }
+}
